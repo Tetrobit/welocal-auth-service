0b015395f16a68a17a87837183a53bbf657b80b0
russian_proger <sagitov.2806@mail.ru>
2025-04-03 13:26:02 +0300

    feat: add Dockerfile, change default ports

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -11,7 +11,7 @@ const options: swaggerJsdoc.Options = {
         },
         servers: [
             {
-                url: 'http://localhost:8088',
+                url: 'http://localhost:10409',
                 description: 'Development server',
             },
         ],
